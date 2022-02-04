320bc05d1865cf4c7649b61e70ed8fe7c89e4136
Ricardo Bohadana Martins <cacolorde@gmail.com>
2022-02-04 15:15:07 -0300

    updated Tests

--- imdb_lib_test.py
+++ imdb_lib_test.py
@@ -1,6 +1,5 @@
-from sqlite3 import DatabaseError
 import unittest
-from imdb_lib import httpIMDB
+from session import httpIMDB
 from unittest import TestCase
 import pandas as pd
 
@@ -12,7 +11,7 @@ class TestClass(TestCase):
         self.Id = 'tt0993846'
 
     def tearDown(self) -> None:
-        self.assertIsNotNone(self.df)
+        # self.assertIsNotNone(self.df)
         self.assertFalse(self.df.empty)
         self.assertFalse(self.df.isnull().values.any())
         self.assertEqual(sum(self.df.isna().sum().to_list()), 0)
@@ -38,16 +37,27 @@ class TestClass(TestCase):
 
 
     def test_getTopRatedMovies(self):
-        self.df = self.session.getTopRatedMovies()
-
-        self.assertEqual(len(self.df), 250)
-
+        self.df = self.session.getTopRatedMovies()    
+  
 
     def test_getTitleDetails(self):
         self.df = self.session.getTitleDetails(self.Id)
         
         cols = self.df.columns.to_list()
         self.assertEqual(cols, 10)
+
+        # assert all columns exist
+        cols = len(self.df.columns.to_list())
+        self.assertEqual(cols, 10)
+        
+        # assert duration is a tuple with length == 2
+        duration = self.df.duration.to_list()
+        bool_d = [True if len(d) == 2 else False for d in duration]
+        self.assertNotIn(False, bool_d)
+        
+        # assert vals of duration arent NaN or simillar
+        for v1,v2 in duration:
+            self.assertIsNotNone(v1+v2)
 
 
     def test_searchTitle(self):
